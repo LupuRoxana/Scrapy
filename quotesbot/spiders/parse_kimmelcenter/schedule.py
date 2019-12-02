695a7f8615a02ebfdc712b9bee1afb0c3a1200cd
Madincea <43602507+Madincea@users.noreply.github.com>
2019-12-02 19:23:21 +0200

    check if it is free event (do nothing)

--- quotesbot/spiders/parse_kimmelcenter/schedule.py
+++ quotesbot/spiders/parse_kimmelcenter/schedule.py
@@ -3,11 +3,9 @@ import logging
 import scrapy
 
 from quotesbot.spiders.utils.http import JSONRequest
-
 from pprint import pprint
 
 logger = logging.getLogger(__name__)
-
 
 class ScheduleParser(object):
     def __init__(self, url):
@@ -40,46 +38,38 @@ class ScheduleParser(object):
             self.logger.exception(e)
             return None
 
-
-
     @staticmethod
     def extract_schedule_page(response):
         data = get_data(response,'')
         for dt in data:
             event = dt['performance']
-            street_number = event['venueAddress'].split(' ', 1)
+            if event["category"] != "Free at the Kimmel":
+                street_number = event['venueAddress'].split(' ', 1)
+                buy_url = f"https://tickets.philorch.org/SmartSeat/Index?itemNumber={event['id']}#/seatmap"
+                url = "https://tickets.philorch.org/api/seating/GetSeatmap"
 
-            buy_url = f"https://tickets.philorch.org/SmartSeat/Index?itemNumber={event['id']}#/seatmap"
-            url = "https://tickets.philorch.org/api/seating/GetSeatmap"
-
-            if url != "https://tickets.philorch.org/api/seating/GetSeatmap":
-                proxies = ""
-            else:
-                proxies = "http://e5a4a23393fd42f6b538816e3f8fb0e9:@proxy.crawlera.com:8010/"
-            data = {"itemType":0,
-                    "itemId":event['id'],
-                    "minPrice":"null",
-                    "maxPrice":"null",
-                    "allowSeparatedSeats":"false",
-                    "priceTypeQuantities":[],
-                    "venueLevels":[],
-                    "allowAisleAccessSelected":"false",
-                    "additionalSeatingOptions":[],
-                    "seatMapId":"","language":"null",
-                    "seatsToIgnore":[],
-                    "isSyosOnly":"false"}
-            headers = {"content-type": "application/json"}
-            ev = EventParser(event['name'], event['id'], buy_url, event['eventDate'], event['venueName'], street_number[0])
-            yield JSONRequest(url, method="POST", headers = headers, data = data, meta= {'proxy': proxies}, callback=ev.parse_performance_seats)
-
-
-
-
+                if url != "https://tickets.philorch.org/api/seating/GetSeatmap":
+                    proxies = ""
+                else:
+                    proxies = "http://e5a4a23393fd42f6b538816e3f8fb0e9:@proxy.crawlera.com:8010/"
+                data = {"itemType":0,
+                        "itemId":event['id'],
+                        "minPrice":"null",
+                        "maxPrice":"null",
+                        "allowSeparatedSeats":"false",
+                        "priceTypeQuantities":[],
+                        "venueLevels":[],
+                        "allowAisleAccessSelected":"false",
+                        "additionalSeatingOptions":[],
+                        "seatMapId":"","language":"null",
+                        "seatsToIgnore":[],
+                        "isSyosOnly":"false"}
+                headers = {"content-type": "application/json"}
+                ev = EventParser(event['name'], event['id'], buy_url, event['eventDate'], event['venueName'], street_number[0])
+                yield JSONRequest(url, method="POST", headers = headers, data = data, meta= {'proxy': proxies}, callback=ev.parse_performance_seats)
 
 class EventParser(object):
-
     def __init__(self,name, performance_id, buy_url, startDateTimeAsString, venue_Name, street_number):
-
         self.logger = logger
         self.event = {
             "name": name,
@@ -98,6 +88,7 @@ class EventParser(object):
             'countryCode': 'USA',
             'countryName': 'USA'
         }
+        
     def __str__(self):
         return f"{self.event}"
 
